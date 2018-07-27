1ed396debcce1612b9696d76ffa21fd6e61913d0
Haris Khan <haris.khan@gems.techverx.com>
2018-07-27 17:00:36 +0500

    change in seeder

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -13,7 +13,6 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'admin']);
         Role::create(['name' => 'user']);
         Role::create(['name' => 'contributor']);
     }
