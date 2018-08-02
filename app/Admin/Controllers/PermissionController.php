c9e64415bf5e0d26d2054cdba555cbb129b6669d
Haris Khan <haris.khan@gems.techverx.com>
2018-08-02 12:35:57 +0500

    Roles and Permissions of simple User in Admin

---
+++ app/Admin/Controllers/PermissionController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use App\User;
+use App\Services\UserServices;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header(trans('Permissions'));
+            $content->description(trans('Permission List'));
+            $content->body($this->grid()->render());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     *
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+            $content->header('Permissions');
+            $content->description('Edit Permission');
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('Permissions');
+            $content->description('Create new permission');
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(Permission::class, function (Grid $grid) {
+            $grid->id('ID')->sortable();
+            $grid->name()->sortable();
+            $grid->column('created_at','Created at')->sortable();
+            $grid->column('updated_at','Last Modified at')->sortable();
+            $grid->filter(function ($filter){
+                $filter->like('name');
+            });
+            $grid->actions(function (Grid\Displayers\Actions $actions) {
+//                $actions->append('<a href="/"><i class="fa fa-eye"></i></a>');
+            });
+            $grid->tools(function (Grid\Tools $tools) {
+                $tools->batch(function (Grid\Tools\BatchActions $actions) {
+                    $actions->disableDelete();
+                });
+            });
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    public function form()
+    {
+        return Admin::form(Permission::class, function (Form $form) {
+            $form->display('id', 'ID');
+            $form->text('name', trans('Name'))->rules('required')->placeholder('Enter Name...');
+        });
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->edit($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id)
+    {
+        return $this->form()->update($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $role = Permission::find($id);
+        if ($role->delete()) {
+            return response()->json([
+                'status'  => true,
+                'message' => trans('admin.delete_succeeded'),
+            ]);
+        } else {
+            return response()->json([
+                'status'  => false,
+                'message' => trans('admin.delete_failed'),
+            ]);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        return $this->form()->store();
+    }
+
+}
