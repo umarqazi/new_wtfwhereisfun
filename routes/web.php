581d0aabcecf115a9b546aa0957cc9823603d163
jazijaved93 <jazib.javed@gems.techverx.com>
2019-01-04 16:01:12 +0500

    Exception Handling enabled

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('checkout', 'PaymentController@checkout');
     Route::post('validate-checkout', 'PaymentController@validateCheckout');
     Route::post('checkout/proceed', 'PaymentController@stripeCheckout');
-    Route::post('checkout/success/{orderId}', 'PaymentController@successfulCheckout');
+    Route::get('checkout/success/{orderId}', 'PaymentController@successfulCheckout');
     Route::post('checkout/notify', 'PaymentController@notifyCheckout');
     Route::get('checkout/cancel', 'PaymentController@cancelCheckout');
     Route::post('checkout/stripe', 'PaymentController@stripeCheckout');
